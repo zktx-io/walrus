cc81cf30a206cbbf5ac31af2f367faaf5455f3c7
Karl Wuest <5716112+karlwuest@users.noreply.github.com>
2024-03-28 11:27:47 +0100

    chore: use temp build directories when compiling move package for tests (#185)

--- crates/walrus-e2e-tests/src/lib.rs
+++ crates/walrus-e2e-tests/src/lib.rs
@@ -4,12 +4,7 @@
 use std::path::PathBuf;
 
 use anyhow::{anyhow, ensure, Result};
-use sui_move_build::{
-    build_from_resolution_graph,
-    gather_published_ids,
-    BuildConfig,
-    PackageDependencies,
-};
+use sui_move_build::{BuildConfig, PackageDependencies};
 use sui_sdk::{
     rpc_types::SuiTransactionBlockEffectsAPI,
     types::base_types::ObjectID,
@@ -17,18 +12,12 @@ use sui_sdk::{
 };
 
 pub fn compile_package(package_path: PathBuf) -> (PackageDependencies, Vec<Vec<u8>>) {
-    let mut build_config = BuildConfig::default();
-    build_config.config.install_dir = Some(package_path.clone());
-    build_config.config.lock_file = Some(package_path.join("Move.lock"));
-    let resolution_graph = build_config
-        .resolution_graph(&package_path)
-        .expect("Resolution failed");
-    let (_, dependencies) = gather_published_ids(&resolution_graph);
-    let compiled_package =
-        build_from_resolution_graph(package_path, resolution_graph, false, false)
-            .expect("Compiling package failed");
+    let build_config = BuildConfig::new_for_testing();
+    let compiled_package = build_config
+        .build(package_path)
+        .expect("Building package failed");
     let compiled_modules = compiled_package.get_package_bytes(false);
-    (dependencies, compiled_modules)
+    (compiled_package.dependency_ids, compiled_modules)
 }
 
 pub fn contract_path(contract: &str) -> anyhow::Result<PathBuf> {
