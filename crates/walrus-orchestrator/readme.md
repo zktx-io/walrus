6081fbf494caa9a5531953fb1d82e3564fe12558
Alberto Sonnino <alberto@sonnino.com>
2024-04-09 09:56:34 -0400

    test: deploy remote testbed (#223)

---
+++ crates/walrus-orchestrator/readme.md
@@ -0,0 +1,96 @@
+# Orchestrator
+
+The Orchestrator crate provides facilities for quickly deploying and benchmarking this codebase in a
+geo-distributed environment. Please note that it is not intended for production deployments or as an
+indicator of production engineering best practices. Its purpose is to facilitate research projects
+by allowing benchmarking of (variants of) the codebase and analyzing performance and robustness.
+
+This guide provides a step-by-step explanation of how to run geo-distributed benchmarks on either
+[Vultr](http://vultr.com) or [Amazon Web Services (AWS)](http://aws.amazon.com).
+
+## Step 1. Set up cloud provider credentials
+
+To enable programmatic access to your cloud provider account from your local machine, you need to
+set up your cloud provider credentials. These credentials authorize your machine to create, delete,
+and edit instances programmatically on your account.
+
+### Setting up Vultr credentials
+
+1. Find your ['Vultr token'](https://www.vultr.com/docs/).
+2. Create a file `~/.vultr` and add your access token as the file's content:
+
+```text
+YOUR_ACCESS_TOKEN
+```
+
+### Setting up AWS credentials
+
+1. Find your ['access key id' and 'secret access
+   key'](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html#cli-configure-quickstart-creds).
+2. Create a file `~/.aws/credentials` with the following content:
+
+```text
+[default]
+aws_access_key_id = YOUR_ACCESS_KEY_ID
+aws_secret_access_key = YOUR_SECRET_ACCESS_KEY
+```
+
+Do not specify any AWS region in that file, as the scripts need to handle multiple regions
+programmatically.
+
+## Step 2. Specify the testbed configuration
+
+Create a file called `settings.yaml` that contains all the configuration parameters for the testbed
+deployment. You can find an example file at [`./assets/settings-template.yaml`][./assets/settings-template.yaml].
+
+The documentation of the `Settings` struct in `./src/settings.rs` provides detailed information
+about each field and indicates which ones are optional. If you're working with a private GitHub
+repository, you can include a [private access
+token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens)
+in the repository URL. For example, if your access token is
+`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`, the repository URL should be formatted as follows:
+
+```yaml
+repository:
+  url: http://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@github.com/mystenlabs/walrus.git
+  commit: main
+```
+
+## Step 3. Create a testbed
+
+The `orchestrator` binary provides various functionalities for creating, starting, stopping, and
+destroying instances. You can use the following command to boot 2 instances per region (if the
+settings file specifies 10 regions, as shown in the example above, a total of 20 instances will be
+created):
+
+```bash
+cargo run --bin orchestrator -- testbed deploy --instances 2
+```
+
+To check the current status of the testbed instances, use the following command:
+
+```bash
+cargo run --bin orchestrator testbed status
+```
+
+Instances listed with a green number are available and ready for use, while instances listed with a
+red number are stopped.
+
+When utilizing a testbed on AWS, a common approach involves configuring the desired parameters and
+maintaining all machines in a stopped state until necessary. This strategy helps control costs, as
+keeping AWS instances stopped typically incurs minimal expenses, while starting them can lead to
+significant charges. It's important to note that cost dynamics may vary among providers, so it's
+advisable to thoroughly examine the associated costs for each operation before initiating
+benchmarks.
+
+## Step 4. Deploying a testbed
+
+TBD
+
+## Step 5. Running benchmarks
+
+TBD
+
+## Step 6. Monitoring
+
+TBD
---
+++ crates/walrus-orchestrator/src/main.rs
@@ -0,0 +1,159 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Orchestrator entry point.
+
+use clap::Parser;
+use client::{aws::AwsClient, vultr::VultrClient, ServerProviderClient};
+use eyre::Context;
+use settings::{CloudProvider, Settings};
+use testbed::Testbed;
+
+mod client;
+mod display;
+mod error;
+mod settings;
+mod ssh;
+mod testbed;
+
+/// The orchestrator command line options.
+#[derive(Parser, Debug)]
+#[command(author, version, about = "Testbed orchestrator", long_about = None)]
+#[clap(rename_all = "kebab-case")]
+pub struct Opts {
+    /// The path to the settings file. This file contains basic information to deploy testbeds
+    /// and run benchmarks such as the url of the git repo, the commit to deploy, etc.
+    #[clap(
+        long,
+        value_name = "FILE",
+        default_value = "orchestrator/assets/settings-template.yaml",
+        global = true
+    )]
+    settings_path: String,
+
+    /// The type of operation to run.
+    #[clap(subcommand)]
+    operation: Operation,
+}
+
+/// The type of operation to run.
+#[derive(Parser, Debug)]
+#[clap(rename_all = "kebab-case")]
+pub enum Operation {
+    /// Read or modify the status of the testbed.
+    Testbed {
+        /// The action to perform on the testbed.
+        #[clap(subcommand)]
+        action: TestbedAction,
+    },
+    /// Deploy nodes and run a benchmark on the specified testbed.
+    Benchmark,
+}
+
+/// The action to perform on the testbed.
+#[derive(Parser, Debug)]
+#[clap(rename_all = "kebab-case")]
+pub enum TestbedAction {
+    /// Display the testbed status.
+    Status,
+
+    /// Deploy the specified number of instances in all regions specified by in the setting file.
+    Deploy {
+        /// Number of instances to deploy.
+        #[clap(long)]
+        instances: usize,
+
+        /// The region where to deploy the instances. If this parameter is not specified, the
+        /// command deploys the specified number of instances in all regions listed in the
+        /// setting file.
+        #[clap(long)]
+        region: Option<String>,
+    },
+
+    /// Start at most the specified number of instances per region on an existing testbed.
+    Start {
+        /// Number of instances to deploy.
+        #[clap(long, default_value_t = 10)]
+        instances: usize,
+    },
+
+    /// Stop an existing testbed (without destroying the instances).
+    Stop,
+
+    /// Destroy the testbed and terminate all instances.
+    Destroy,
+}
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+    let opts: Opts = Opts::parse();
+
+    // Load the settings files.
+    let settings = Settings::load(&opts.settings_path).wrap_err("Failed to load settings")?;
+
+    match &settings.cloud_provider {
+        CloudProvider::Aws => {
+            // Create the client for the cloud provider.
+            let client = AwsClient::new(settings.clone()).await;
+
+            // Execute the command.
+            run(settings, client, opts).await
+        }
+        CloudProvider::Vultr => {
+            // Create the client for the cloud provider.
+            let token = settings
+                .load_token()
+                .wrap_err("Failed to load cloud provider's token")?;
+            let client = VultrClient::new(token, settings.clone());
+
+            // Execute the command.
+            run(settings, client, opts).await
+        }
+    }
+}
+
+async fn run<C: ServerProviderClient>(
+    settings: Settings,
+    client: C,
+    opts: Opts,
+) -> eyre::Result<()> {
+    // Create a new testbed.
+    let mut testbed = Testbed::new(settings.clone(), client)
+        .await
+        .wrap_err("Failed to crate testbed")?;
+
+    match opts.operation {
+        Operation::Testbed { action } => match action {
+            // Display the current status of the testbed.
+            TestbedAction::Status => testbed.status(),
+
+            // Deploy the specified number of instances on the testbed.
+            TestbedAction::Deploy { instances, region } => testbed
+                .deploy(instances, region)
+                .await
+                .wrap_err("Failed to deploy testbed")?,
+
+            // Start the specified number of instances on an existing testbed.
+            TestbedAction::Start { instances } => testbed
+                .start(instances)
+                .await
+                .wrap_err("Failed to start testbed")?,
+
+            // Stop an existing testbed.
+            TestbedAction::Stop => testbed.stop().await.wrap_err("Failed to stop testbed")?,
+
+            // Destroy the testbed and terminal all instances.
+            TestbedAction::Destroy => testbed
+                .destroy()
+                .await
+                .wrap_err("Failed to destroy testbed")?,
+        },
+
+        // Run benchmarks.
+        Operation::Benchmark => {
+            todo!("Alberto: Implement the benchmark operation. (#222)")
+        }
+    }
+    Ok(())
+}
