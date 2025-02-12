f48aab99781f1892e27b594dfc18421a00523dcd
Zhe Wu <halfprice@users.noreply.github.com>
2025-02-11 21:27:52 -0800

    feat: add dbtool to count total number of certified blobs (#1564)
    Add dbtool to scan certified blobs

--- crates/walrus-service/src/node/dbtool.rs
+++ crates/walrus-service/src/node/dbtool.rs
@@ -13,7 +13,7 @@ use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 use sui_types::base_types::ObjectID;
 use typed_store::rocks::be_fix_int_ser;
-use walrus_core::BlobId;
+use walrus_core::{BlobId, Epoch};
 
 use super::events::PositionedStreamEvent;
 use crate::node::{
@@ -22,6 +22,7 @@ use crate::node::{
         blob_info_cf_options,
         per_object_blob_info_cf_options,
         BlobInfo,
+        BlobInfoApi,
         PerObjectBlobInfo,
         AGGREGATE_BLOB_INFO_COLUMN_FAMILY_NAME,
         PER_OBJECT_BLOB_INFO_COLUMN_FAMILY_NAME,
@@ -72,6 +73,16 @@ pub enum DbToolCommands {
         /// Object ID to read.
         object_id: ObjectID,
     },
+
+    /// Count the number of certified blobs in the RocksDB database.
+    CountCertifiedBlobs {
+        /// Path to the RocksDB database directory.
+        #[clap(long)]
+        db_path: PathBuf,
+        /// Epoch the blobs are in certified status.
+        #[clap(long)]
+        epoch: Epoch,
+    },
 }
 
 impl DbToolCommands {
@@ -88,6 +99,7 @@ impl DbToolCommands {
             Self::ReadObjectBlobInfo { db_path, object_id } => {
                 read_object_blob_info(db_path, object_id)
             }
+            Self::CountCertifiedBlobs { db_path, epoch } => count_certified_blobs(db_path, epoch),
         }
     }
 }
@@ -182,5 +194,46 @@ fn read_object_blob_info(db_path: PathBuf, object_id: ObjectID) -> Result<()> {
         println!("PerObjectBlobInfo not found");
     }
 
+    Ok(())
+}
+
+fn count_certified_blobs(db_path: PathBuf, epoch: Epoch) -> Result<()> {
+    let blob_info_options = blob_info_cf_options(&DatabaseConfig::default());
+    let db = DB::open_cf_with_opts_for_read_only(
+        &RocksdbOptions::default(),
+        db_path,
+        [(AGGREGATE_BLOB_INFO_COLUMN_FAMILY_NAME, blob_info_options)],
+        false,
+    )?;
+
+    let cf = db
+        .cf_handle(AGGREGATE_BLOB_INFO_COLUMN_FAMILY_NAME)
+        .expect("Aggregate blob info column family should exist");
+
+    // Scan all the blob info and count the certified ones
+    let iter = db.iterator_cf(&cf, rocksdb::IteratorMode::Start);
+
+    let mut certified_count = 0;
+    let mut scan_count = 0;
+    for blob_info_raw in iter {
+        let (_key, value) = blob_info_raw?;
+        let blob_info: BlobInfo = bcs::from_bytes(&value)?;
+        if blob_info.is_certified(epoch) {
+            certified_count += 1;
+        }
+
+        scan_count += 1;
+        if scan_count % 10000 == 0 {
+            println!(
+                "Scanned {} blobs. Found {} certified blobs",
+                scan_count, certified_count
+            );
+        }
+    }
+
+    println!(
+        "Number of certified blobs: {}. Scanned {} blobs",
+        certified_count, scan_count
+    );
     Ok(())
 }
