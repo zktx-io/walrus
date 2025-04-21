eb57855d543aace2f95e4ec78947670b58aa1d70
Will Bradley <will.bradley@mystenlabs.com>
2025-04-21 15:40:14 -0600

    chore: add a "crumb" macro to walrus_utils for help in debugging (#2000)
    * chore: add a "crumb" macro to walrus_utils for help in debugging

--- crates/walrus-utils/src/lib.rs
+++ crates/walrus-utils/src/lib.rs
@@ -25,3 +25,29 @@ pub mod tests {
         LOCK.get_or_init(Mutex::default)
     }
 }
+
+/// A macro to print a crumb of information to the console. This is useful for debugging.
+#[macro_export]
+macro_rules! crumb {
+    ($($arg:tt)+) => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+        let name = type_name_of(f);
+        eprintln!(
+            "{}:{}: crumb: [{:?}]: {}: {}",
+            file!(),
+            line!(),
+            std::thread::current().id(),
+            name.strip_suffix("::f").unwrap(),
+            format!($($arg)+),
+        );
+    }};
+    ($expr:literal) => {{
+        $crate::crumb!("{}", $expr);
+    }};
+    () => {{
+        $crate::crumb!("you left a crumb here.");
+    }};
+}
